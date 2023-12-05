6f80c67f7aad759944c58c4b68dbaa413bdcab32
Danny Aldham <daniel.aldham@gmail.com>
2023-12-05 22:38:54 +0100

    day 2 complete :)

--- day2.py
+++ day2.py
@@ -35,4 +35,35 @@ with open(file, 'r') as f:
             #print(game_num)
             sum += game_num
         # print(sum)
+    #print(sum)
+
+with open(file, 'r') as f:
+    sum = 0
+    for line in f:
+        line = line.strip()
+        line = line.split(':')
+        sets = line[1].split(';')
+        max_blue = 0
+        max_red = 0
+        max_green = 0
+        for set in sets:
+            set = set.split(',')
+            for group in set:
+                if 'blue' in group:
+                    num = group.split()[0]
+                    num = int(num)
+                    if num > max_blue:
+                        max_blue = num
+                if 'red' in group:
+                    num = group.split()[0]
+                    num = int(num)
+                    if num > max_red:
+                        max_red = num
+                if 'green' in group:
+                    num = group.split()[0]
+                    num = int(num)
+                    if num > max_green:
+                        max_green = num
+        power = max_green * max_blue * max_red
+        sum += power
     print(sum)
